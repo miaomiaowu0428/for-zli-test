cf710dbb60b842914e51b5243493008825922ab5
qiumiaomiao <2268428576@qq.com>
2024-10-08 23:28:55 +0800

    change to rocket

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use colored::Colorize;
-use zino::prelude::*;
 
-fn main() {
-    println!("3{}" , "Project Running".blue());
-    zino::Cluster::boot().run();
+
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
 }
 
+#[launch]
+fn rocket() -> _ {
+    println!("3{}" , "Project Running".blue());
+    let figment = rocket::Config::figment()
+        .merge(("address", "0.0.0.0"))
+        .merge(("port",6080));
 
+    rocket::custom(figment).mount("/", routes![index])
+}
