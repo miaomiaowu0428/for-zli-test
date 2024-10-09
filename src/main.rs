86b0801349f41a95d15c5ebc78ddcb89b54bcd2b
qiumiaomiao <2268428576@qq.com>
2024-10-09 13:46:17 +0800

    change to 6080

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn rocket() -> _ {
     println!("3{}" , "Project Running".blue());
     let figment = rocket::Config::figment()
         .merge(("address", "0.0.0.0"))
-        .merge(("port",443));
+        .merge(("port",6080));
 
     rocket::custom(figment).mount("/", routes![index])
 }
